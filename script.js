4c538b89d7f9d53863e06b599efa2224125c18e0
Calvin Roberts <roberts.calvin85@gmail.com>
2022-10-19 23:27:58 +0100

    Refactor function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let container = document.querySelector('.container');
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 100; i++) {
   let dot = document.createElement('div');
   dot.classList.add('element');
   container.appendChild(dot);
